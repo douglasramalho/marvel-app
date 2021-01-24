328216c7b082127e5273f42ad6d3b22e70cb8130
Douglas Motta <tibeca@gmail.com>
2021-01-24 17:43:34 +0000

    Configura navegação principal

---
+++ app/src/main/java/com/example/marvelapp/ui/characters/CharactersFragment.kt
@@ -0,0 +1,6 @@
+package com.example.marvelapp.ui.characters
+
+import androidx.fragment.app.Fragment
+import com.example.marvelapp.R
+
+class CharactersFragment : Fragment(R.layout.fragment_characters)
